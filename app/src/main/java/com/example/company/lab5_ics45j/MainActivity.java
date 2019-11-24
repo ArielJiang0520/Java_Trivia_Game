a3327632eb1bd6e43f8ded00214db08c6a7ad8bf
Andrew Nguyen <andrewtnguyen49@gmail.com>
2019-11-24 13:53:45 -0800

    Initial commit

---
+++ app/src/main/java/com/example/company/lab5_ics45j/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.company.lab5_ics45j;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
