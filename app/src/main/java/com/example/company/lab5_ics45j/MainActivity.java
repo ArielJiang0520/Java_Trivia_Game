71126019f9b2895fda5998e67545fa32104760c1
ArielJiang0520 <32921432+ArielJiang0520@users.noreply.github.com>
2019-11-26 20:00:49 -0800

    firebase connected

--- app/src/main/java/com/example/company/lab5_ics45j/MainActivity.java
+++ app/src/main/java/com/example/company/lab5_ics45j/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.company.lab5_ics45j;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        Toast.makeText(this, "hello successfully added.", Toast.LENGTH_LONG).show();
     }
 }
