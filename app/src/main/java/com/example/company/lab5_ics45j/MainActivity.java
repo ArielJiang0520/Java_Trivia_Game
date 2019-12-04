6ce0684c2a431027490e6240a94f2139113c0fdf
ArielJiang0520 <32921432+ArielJiang0520@users.noreply.github.com>
2019-12-03 17:17:40 -0800

    read from database

--- app/src/main/java/com/example/company/lab5_ics45j/MainActivity.java
+++ app/src/main/java/com/example/company/lab5_ics45j/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.company.lab5_ics45j;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,10 +34,33 @@ public class MainActivity extends AppCompatActivity {
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference("questions");
 
-        myRef.setValue("Hello, World0!");
+        //myRef.setValue("Hello, World!");
 
-        Toast.makeText(this, "hello successfully added.", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "hello successfully added.", Toast.LENGTH_LONG).show();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) {
+                        String v = childSnapshot.child("question").getValue(String.class);
+                        if (v != null)
+                            Log.d(TAG, v);
+                        else
+                            Log.d(TAG, "value is NULL");
+                    }
+                }
+                else
+                    Log.d(TAG, "snapshot does not exist.");
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.");
+            }
+        });
+
     }
 }
